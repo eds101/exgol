46577e8e89989d555e6b6f5dd3855aeccc7fd7c9
schmidt.eric <schmidt.eric@13686cf8-1cff-11de-a958-c9a343b5b891>
2009-04-09 12:38:31 +0000

    added two fields

--- branches/sikarwar/src/columbia/exgol/Simulation.java
+++ branches/sikarwar/src/columbia/exgol/Simulation.java
@@ -5,10 +5,20 @@ import java.util.Hashtable;
 import java.awt.Color;
 
 public class Simulation {
+	// Init section
 	public Hashtable<String, Color> classes;
 	public Vector<String> states;
 	public Vector<Integer> gridsize;
 	public GridType gridtype;
+
+	// Transition section
 	public Vector<Trans> trans;
 	public Vector<TransRule> transrule;
+	
+	// Simulation section
+	public Cell[] grid1d;
+	public Cell[][] grid2d;
+	public Cell[][][] grid3d;
+	public Vector<TransRule> simrules;
+	public int generations;
 }
