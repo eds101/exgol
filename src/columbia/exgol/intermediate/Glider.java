fd135a3ca7035bc2989000b87914fe87dd187eef
akshat.sikarwar <akshat.sikarwar@13686cf8-1cff-11de-a958-c9a343b5b891>
2009-05-08 09:00:15 +0000

    grammar wtfsvn status added glider and blinker

--- src/columbia/exgol/intermediate/Glider.java
+++ src/columbia/exgol/intermediate/Glider.java
@@ -2,13 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package columbia.exgol.intermediate;
 
 /**
  *
  * @author sikarwar
  */
-public enum PopulateType {
-	POISSON, NORMAL, LINE, CIRCLE, RECTANGLE, DOT, GLIDER, BLINKER;
+public class Glider {
+    public static int NW = 0;
+    public static int NE = 1;
+    public static int SW = 2;
+    public static int SE = 3;
 }
-
